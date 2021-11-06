2c7c0e102dc323980c952b269336511884bbbb5a
Omaenoshi <rassvetalov.maksim@mail.ru>
2021-11-07 02:17:26 +0500

    Create Queue

---
+++ RealizationQueue/RealizationQueue/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RealizationQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyQueue<int> test = new MyQueue<int>();
+            Console.WriteLine(test.IsEmpry());
+            test.PutValue(1);
+            test.PutValue(2);
+            test.PutValue(3);
+            test.RemoveValue();
+            test.Print();
+        }
+    }
+}
